4afda17488e4b630915b5c93810b32b857189bfc
SmilingPranjal <140375486+SmilingPranjal@users.noreply.github.com>
2023-07-24 12:10:31 +0530

    Update and rename SALESDDL to SALESDDL.sql

---
+++ SALESDDL.sql
@@ -0,0 +1,11 @@
+CREATE TABLE sales(
+  Transaction_date Timestamp,
+  Product varchar(20),
+  price number,
+  payment_type varchar(20),
+  name varchar(50),
+  city varchar(50),
+  state varchar(50),
+  country varchar(50),
+  us_zip number
+);
