4bc13458408b9c5c10b429b4db1186d90ad7c2bf
SmilingPranjal <140375486+SmilingPranjal@users.noreply.github.com>
2023-07-31 16:11:21 +0530

    Create DDL.sql

---
+++ scd2/DDL.sql
@@ -0,0 +1,10 @@
+
+CREATE TABLE SCD2
+(
+    ID                  varchar2(6),
+    NAME                varchar2(25),
+    ABOUT               varchar2(50),
+    CURRENT_FLAG        char(1),
+    EFFECTIVE_FROM_DATE date,
+    EFFECTIVE_TO_DATE   date
+);
